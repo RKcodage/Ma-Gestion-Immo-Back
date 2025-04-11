b48e4b8816b9bc96a9f52c4ba6807382a2b7cd7b
RayanKabra <rkabra.dev@gmail.com>
2025-04-11 17:54:18 +0200

    isAuthenticad on userId route

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const isAuthenticated = require("../middlewares/isAuthenticated");
 const { getUserById } = require("../controllers/userController");
 
-router.get("/user/:id", getUserById);
+router.get("/user/:id", isAuthenticated, getUserById);
 
 module.exports = router;
