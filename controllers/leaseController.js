0aecc266dba58b91f1884d00b29638fa00930597
RayanKabra <rkabra.dev@gmail.com>
2025-05-04 18:32:59 +0200

    Update and delete routes

--- controllers/leaseController.js
+++ controllers/leaseController.js
@@ -104,11 +104,52 @@ const getLeasesByOwner = async (req, res) => {
     res.status(200).json(leasesWithProperty);
   } catch (error) {
     console.error("Error fetching leases :", error.message);
-    res.status(500).json({ message: "Server error during leases fetching." });
+    res.status(500).json({ message: "Server error during leases fetching" });
+  }
+};
+
+// Update lease
+const updateLease = async (req, res) => {
+  try {
+    const { leaseId } = req.params;
+    const updateData = req.body;
+
+    const updatedLease = await Lease.findByIdAndUpdate(leaseId, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedLease) {
+      return res.status(404).json({ message: "Lease not found" });
+    }
+
+    res.status(200).json(updatedLease);
+  } catch (error) {
+    console.error("Update error :", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Delete lease
+const deleteLease = async (req, res) => {
+  try {
+    const { leaseId } = req.params;
+
+    const deletedLease = await Lease.findByIdAndDelete(leaseId);
+    if (!deletedLease) {
+      return res.status(404).json({ message: "Lease not found" });
+    }
+
+    res.status(200).json({ message: "Lease successfully erased" });
+  } catch (error) {
+    console.error("Lease deleting error :", error.message);
+    res.status(500).json({ message: "Server error while deleting lease" });
   }
 };
 
 module.exports = {
   createLease,
   getLeasesByOwner,
+  updateLease,
+  deleteLease,
 };
--- routes/lease.js
+++ routes/lease.js
@@ -4,9 +4,13 @@ const isAuthenticated = require("../middlewares/isAuthenticated");
 const {
   createLease,
   getLeasesByOwner,
+  updateLease,
+  deleteLease,
 } = require("../controllers/leaseController");
 
 router.post("/lease", isAuthenticated, createLease);
 router.get("/lease/:ownerId", isAuthenticated, getLeasesByOwner);
+router.put("/lease/:leaseId", updateLease);
+router.delete("/lease/:leaseId", deleteLease);
 
 module.exports = router;
