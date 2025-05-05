6fbdf5f0acb5f75470d5447fef510d0ed9a17060
RayanKabra <rkabra.dev@gmail.com>
2025-05-05 12:59:14 +0200

    Cloudinary config

---
+++ config/cloudinary.js
@@ -0,0 +1,36 @@
+const { v2: cloudinary } = require("cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+require("dotenv").config();
+
+// Cloudinary Config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Storage for users avatars
+const avatarStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "avatars",
+    allowed_formats: ["jpg", "jpeg", "png"],
+    transformation: [{ width: 300, height: 300, crop: "fill" }],
+  },
+});
+
+// Storage for leases documents
+const leaseDocStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "leases-documents",
+    allowed_formats: ["jpg", "jpeg", "png", "pdf"],
+    resource_type: "auto", // important pour permettre pdf/images
+  },
+});
+
+module.exports = {
+  cloudinary,
+  avatarStorage,
+  leaseDocStorage,
+};
