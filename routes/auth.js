1d35c52989212ccda52e2724a0f2465950c4f53b
RayanKabra <rkabra.dev@gmail.com>
2025-05-09 16:20:38 +0200

    forgot and reset routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login } = require("../controllers/authController");
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/authController");
 
 router.post("/user/signup", signup);
 router.post("/user/login", login);
+
+// Forget password
+router.post("/user/forgot-password", forgotPassword);
+router.post("/user/reset-password", resetPassword);
 
 module.exports = router;
