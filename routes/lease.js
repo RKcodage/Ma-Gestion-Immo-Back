c6a4266300b2aa1f387454ae5f2c90750ded42f4
RayanKabra <rkabra.dev@gmail.com>
2025-05-10 20:12:08 +0200

    Get payments historic by lease

--- routes/lease.js
+++ routes/lease.js
@@ -8,12 +8,14 @@ const {
   updateLease,
   deleteLease,
   getUpcomingPayments,
+  getPaymentsHistoric,
 } = require("../controllers/leaseController");
 
 router.post("/lease", isAuthenticated, createLease);
 router.get("/lease/:ownerId", isAuthenticated, getLeasesByOwner);
 router.get("/leases", isAuthenticated, getLeasesByRole);
 router.get("/leases/upcoming-payments", isAuthenticated, getUpcomingPayments);
+router.get("/leases/historic", isAuthenticated, getPaymentsHistoric);
 router.put("/lease/:leaseId", updateLease);
 router.delete("/lease/:leaseId", deleteLease);
 
