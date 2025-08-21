4b80b95fcf89d896c1b7c10ef7173c45c674cc9c
RayanKabra <rkabra.dev@gmail.com>
2025-08-21 21:54:24 +0200

    Helmet installation and use in app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const http = require("http");
 const { initSocket } = require("./socket");
 const connectToDatabase = require("./config/db.js");
@@ -32,6 +33,7 @@ connectToDatabase();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 // Media uploads
 app.use("/uploads", express.static("uploads"));
